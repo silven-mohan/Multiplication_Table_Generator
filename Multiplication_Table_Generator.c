0379200ae9daa9b3e4b6e84b6e4f2d3955e4e503
Silven Mohan <silven667@gmail.com>
2025-06-15 11:11:18 +0530

    Generates Multiplication table for any number

---
+++ Multiplication_Table_Generator.c
@@ -0,0 +1,21 @@
+/** Multiplication Table Generator **/
+
+#include<stdlib.h>
+
+main()
+{
+	int m, n, f, i;
+	system("cls");
+	printf("Enter any number for which you want the Multiplication Table:");
+	scanf("%d", &n);
+	printf("     Multiplication Table of %d", n);
+	printf("\n________________________________");
+	for(i=1;i<=10;i++)
+	{
+		m=n*i;
+		f=n*(i+10);
+		printf("\n |%d x %d = %d \t| %d x %d = %d |", n, i, m, n, i+10, f);
+	}
+	printf("\n________________________________");
+	
+}
